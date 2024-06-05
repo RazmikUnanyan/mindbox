b960120b5daa00b472c53dbd77f81daf0d1ffcea
Razmik <outsource2@omnicampus.io>
2024-06-05 17:23:01 -0500

    ref: tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,13 +6,14 @@ test('add a task and clear', () => {
   render(<App />);
   const inputElement = screen.getByPlaceholderText('What needs to be done?');
   const taskText = 'Новая задача';
-  const taskElement = screen.getByLabelText(taskText) as HTMLInputElement;
 
   // Вводим название задачи
   fireEvent.change(inputElement, { target: { value: taskText } });
-
   // Добавляем задачу
   fireEvent.keyDown(inputElement, { key: 'Enter', code: 13, charCode: 13 });
+
+  //Получаем добавленную задачу
+  const taskElement = screen.getByLabelText(taskText) as HTMLInputElement;
 
   // Проверяем, что задача добавлена
   expect(taskElement).toBeInTheDocument();
