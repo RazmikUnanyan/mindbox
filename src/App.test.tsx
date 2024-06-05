10eba75261abee49aaeb94398a11550d0e558c79
Razmik <outsource2@omnicampus.io>
2024-06-05 17:13:54 -0500

    ref: tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,30 +2,20 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('add a task', () => {
+test('add a task and clear', () => {
   render(<App />);
   const inputElement = screen.getByPlaceholderText('What needs to be done?');
   const taskText = 'Новая задача';
+  const taskElement = screen.getByLabelText(taskText) as HTMLInputElement;
 
   // Вводим название задачи
   fireEvent.change(inputElement, { target: { value: taskText } });
+
   // Добавляем задачу
   fireEvent.keyDown(inputElement, { key: 'Enter', code: 13, charCode: 13 });
 
-  const taskElement = screen.getByText(taskText) as HTMLInputElement;
+  // Проверяем, что задача добавлена
   expect(taskElement).toBeInTheDocument();
-});
-
-
-test('completed a task', () => {
-  render(<App />);
-  const inputElement = screen.getByPlaceholderText('What needs to be done?');
-  const taskText = 'Новая задача';
-
-  fireEvent.change(inputElement, { target: { value: taskText } });
-  fireEvent.keyDown(inputElement, { key: 'Enter', code: 13, charCode: 13 });
-
-  const taskElement = screen.getByLabelText(taskText) as HTMLInputElement;
 
   // Проверяем, что изначально чекбокс не отмечен
   expect(taskElement.checked).toBe(false);
@@ -35,6 +25,13 @@ test('completed a task', () => {
 
   // Проверяем, что после клика чекбокс стал отмеченным
   expect(taskElement.checked).toBe(true);
+
+  // Удаляем выбранную задачу
+  const deleteButton = screen.getByText('Clear completed');
+  fireEvent.click(deleteButton);
+
+  // Проверить, что задача удалена
+  expect(taskElement).not.toBeInTheDocument();
 });
 
 
